f4ed9804de99b538ec548405834ca6b34fcddf04
Christian Schlosrich <christian.schlosrich@gmail.com>
2015-10-28 10:05:30 +1100

    Moving middleware and routes to seperate files

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ var express = require('express')
     , app = express()
     , router = express.Router()
     , swig = require('swig')
-    , cookieParser = require('cookie-parser')
-    , bodyParser = require('body-parser')
-    , compression = require('compression')
     , config = require('./lib/config')
     , path = require('path');
 
@@ -19,44 +16,8 @@ swig.setDefaults({
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'lib', 'templates', 'views'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(compression());
-app.use(express.static(path.join(__dirname, '../', config.get('staticContentPath')), {
-    maxAge: (60 * 60 * 24 * 7) * 1000 // 7 days x 1000 because Express middleware expects miliseconds and not seconds
-}));
-
-app.use(function(req, res, next) {
-    res.locals = config.getCurrent();
-    next();
-});
-
-// For a realworld app, you would probably want to seperate app, routes and middleware into seperate files
-router.get('/', require('./lib/templates/views/index'));
-router.get('/1', require('./lib/templates/views/page1'));
-router.get('/2', require('./lib/templates/views/page2'));
-
-app.use(config.get('baseUrl'), router);
-
-app.get('*', function(req, res, next){
-    var err = new Error();
-        err.status = 404;
-        err.url = req.url;
-
-    next(err);
-});
-
-app.use(function(err, req, res, next){
-
-    if(err.status !== 404){
-        return next();
-    }
-
-    res.render('errors/404', {
-        err: err
-    });
-});
+require('./lib/middleware')(app);
+require('./lib/routes/http')(app, router);
 
 var server = app.listen(config.get('port'), function() {
     console.info('server running: ' + JSON.stringify(server.address()));
